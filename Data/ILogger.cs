8e398383af29cef9f376ff655ffdd92e5414e59a
darrenscrine <darrenscrine@gmail.com>
2015-06-24 14:52:28 +0100

    Added customer config files and ILogger

---
+++ Data/ILogger.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Data
+{
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+}
---
+++ DiContainer/App_Start/MvcConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Autofac;
+using DiContainer.Controllers;
+using Autofac.Integration.Mvc;
+using System.Web.Mvc;
+using Autofac.Core;
+using Data;
+namespace DiContainer
+{
+    public static class MvcConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var builder = new Autofac.ContainerBuilder();
+
+            // You can register controllers all at once using assembly scanning...
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // ...or you can register individual controlllers manually.
+            builder.RegisterType<HomeController>().InstancePerRequest();
+
+            builder.RegisterType<Logger2>().As<ILogger>();
+            builder.RegisterType<Repository>().As<IRepository>();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+        }
+    }
+}
