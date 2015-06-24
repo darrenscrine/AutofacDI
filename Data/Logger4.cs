5f2e27c0e9f262d5074e14f4eca0df53714f2b3f
scr33tch <darrenscrine@gmail.com>
2015-06-24 15:52:29 +0100

    Added Logger4

---
+++ Data/Logger4.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace Data
+{
+    public class Logger4 : ILogger
+    {
+        public void Log(string message)
+        {
+            Trace.Write("Written with 'Logger3':" + message);
+        }
+    }
+}
