70b784888f4264075ee692c2d1ab5cf1c2e8be67
scr33tch <darrenscrine@gmail.com>
2015-06-25 11:24:24 +0100

    Added unit tests for Logger

--- Data/ILogger.cs
+++ Data/ILogger.cs
@@ -2,6 +2,7 @@
 {
     public interface ILogger
     {
+        string GetMessage();
         void Log(string message);
     }
 }
--- Data/Logger.cs
+++ Data/Logger.cs
@@ -4,9 +4,17 @@ namespace Data
 {
     public class Logger : ILogger
     {
+        private string _message = "";
+
+        public string GetMessage()
+        {
+            return _message;
+        }
+
         public void Log(string message)
         {
-            Trace.Write("Written with 'Logger':" + message);
+            _message = "Written with 'Logger':" + message;
+            Trace.Write(_message);
         }
     }
 }
